b523eb5c7438a3b43858893e846e8faad113740b
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-20 13:01:33 -0500

    inner join

--- queries.sql
+++ queries.sql
@@ -97,3 +97,31 @@ select name, email, year(now()) - year(birthdate) as age, gender
 from clients
 where gender = 'F'
 and name like '%Lop%';
+
+select b.book_id, b.title, a.author_id, a.name
+from books as b
+inner join authors as a
+    on a.author_id = b.author_id
+where a.author_id between 1 and 5;
+
+select c.name, b.title, a.name, t.type
+from transactions as t
+inner join books as b
+    on t.book_id = b.book_id
+inner join clients as c
+    on t.client_id = c.client_id
+inner join authors as a
+    on b.author_id = a.author_id
+where c.gender = 'F'
+and t.type = 'sell';
+
+select c.name, b.title, a.name, t.type
+from transactions as t
+inner join books as b
+    on t.book_id = b.book_id
+inner join clients as c
+    on t.client_id = c.client_id
+inner join authors as a
+    on b.author_id = a.author_id
+where c.gender = 'M'
+and t.type in ('sell', 'lend');
