0084afcacaeace54c7e1b9510be53428f8ae7f4a
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-20 08:06:50 -0500

    insert command

--- queries.sql
+++ queries.sql
@@ -38,3 +38,32 @@ CREATE TABLE IF NOT EXISTS operations (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+INSERT INTO autors(`name`, nationality)
+    VALUES('Juan Rulfo', 'MEX');
+
+INSERT INTO autors(`name`, nationality)
+    VALUES('Gabriel García Marquez', 'COL');
+
+INSERT INTO autors
+    VALUES('', 'Juan Gabriel Vasquez', 'COL');
+
+INSERT INTO autors(`name`, nationality)
+    VALUES('Juan Gabriel Vasquez', 'COL');
+
+INSERT INTO autors(`name`, nationality)
+    VALUES
+        ('Julio Coltazar', 'ARG'),
+        ('Isabel Allende', 'CHI'),
+        ('Octavio Paz', 'MEX'),
+        ('Juan Carlos Oneti', 'URU');
+        
+INSERT INTO `clients`(`name`, email, birthdate, gender, active) VALUES
+    ('Maria Dolores Gomez','Maria Dolores.95983222J@random.names','1971-06-06','F',1),
+    ('Adrian Fernandez','Adrian.55818851J@random.names','1970-04-09','M',1),
+    ('Maria Luisa Marin','Maria Luisa.83726282A@random.names','1957-07-30','F',1),
+    ('Pedro Sanchez','Pedro.78522059J@random.names','1992-01-31','M',1);
+
+    INSERT INTO `clients`(`name`, email, birthdate, gender, active) VALUES
+        ('Pedro Sanchez','Pedro.78522059J@random.names','1992-01-31','M',0)
+        ON DUPLICATE KEY UPDATE active = VALUES(active);
